9776f869598eed689fd38394d909d03f898e5a88
EltonnJulio <eltonnjulio@gmail.com>
2022-08-23 13:16:56 +0200

    database tables create

--- Kinship-Management.php
+++ Kinship-Management.php
@@ -6,17 +6,23 @@
  *   Version: 0.1.0
  *   Author: Elton Julio
  *   Author URI: https://github.com/eltonnjulio
+ *   Text Domain: Manage_kinships
  */
+if (!defined('ABSPATH')){
+
+    die('Nothing i can do when im called directly!');
+}
 
  class Manage_kinships {
 
 
     public function __construct()
     {
-        add_action( 'init', array($this,'Create_manage_kinships') );
+        add_action( 'init', array($this,'create_manage_kinships') );
     }
     
-    function Create_manage_kinships() {
+    function create_manage_kinships() {
+       
         register_post_type( 'manage_kinships',
             array(
                 'labels' => array(
@@ -27,7 +33,7 @@
                 ),
     
                 'public' => true,
-                'menu_position' => 15,
+                'menu_position' => 16,
                 'supports' => array( 'Name', 'Email', 'kinships'),
                 'taxonomies' => array( '' ),
                 'has_archive' => true
@@ -35,10 +41,64 @@
         );
     }
     
-    function createTables(){
-    
-    
-    
+     function create_peopleTable(){
+        global $wpdb;
+
+        $people_details = $wpdb->prefix.'people_details';
+        $charset = $wpdb->get_charset_collate;
+
+        $people_det = "CREATE TABLE IF NOT EXISTS" .$people_details."(
+            id int AUTO_INCREMENT primary key,
+            name text not null,
+            avatar text not null,
+            age int not null,
+            status int CHECK(status in 0,1),
+            constraint CKT_name_size check ( LENGTH(name) > 5 ),
+            constraint CKT_age check ( age>0 ),
+            UNIQUE KEY thekey (name,age)
+
+        )$charset;";
+
+            require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+
+          dbDelta($people_det);
+
+        //   return var_dump(dbDelta($people_det))
+
+    }
+
+     function create_kinshipTable(){
+        
+        global $wpdb;
+
+        $kinship_details = $wpdb->prefix.'people_kinships';
+        $charset = $wpdb->get_charset_collate;
+
+        $kinship_det = "CREATE TABLE IF NOT EXISTS" .$kinship_details."(
+            id int AUTO_INCREMENT primary key,
+            kinship_name text not null,
+            kinship_owner int not null,
+            kinship int not null,
+            status int CHECK(status in 0,1),
+            constraint fk_kinship_owner (kinship_owner) REFERENCES people_details(id),
+            constraint fk_kinship (kinship) REFERENCES people_details(id),
+            constraint ck_kinship_name CHECK (kinship_name in 'mother','father','son','daughter','brother','sister')
+
+        ) $charset;";
+
+        require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+
+        //  var_dump(dbDelta($kinship_det));
+
+        dbDelta($kinship_det);
+
+    }
+
+
+     function createTables(){
+        
+        $this->create_peopleTable();
+        $this->create_kinshipTable();
     }
     
     public function activate(){
@@ -50,17 +110,19 @@
     
     public function deactivate(){
     
+        // flush_rewrite_rules();
+    
+    }
+    
+    public function uninstall(){
         flush_rewrite_rules();
-    
     }
-    
-    public function uninstall(){}
     
     
     
     }
     
-    if( class_exists('Plugin') ){
+    if( class_exists('Manage_kinships') ){
     
         $plugin = new Manage_kinships();
     
